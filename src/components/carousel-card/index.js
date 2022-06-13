7366a17c7b647422054088f12dc074159b3d058f
Roque Costa <devroque18@gmail.com>
2022-06-13 10:00:51 -0300

    [feat] carousel stores component

--- src/components/carousel-card/index.js
+++ src/components/carousel-card/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './index.css';
 
-export const CarouselCard = () => {
+export const CarouselCard = ({ name, imgSrc }) => {
     return (
         <div className="carousel-card">
-            <figure>
-                <img 
-                    src="http://matheuslessa.com.br/wp-content/uploads/2019/04/vale-a-pena-vender-pelo-ifood.jpg" 
-                    alt="link" 
-                />
-            </figure>
+            <h4>La pasta</h4>
+            <div className="carousel-content">
+                <figure>
+                    <img 
+                        className="store-img"
+                        src="http://matheuslessa.com.br/wp-content/uploads/2019/04/vale-a-pena-vender-pelo-ifood.jpg" 
+                        alt="link" 
+                    />
+                </figure>
+            </div>
         </div>
     );
 }

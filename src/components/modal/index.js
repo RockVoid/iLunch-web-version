b4eaf55ce7769254c6b2e6727bcf54ca2712e0ff
Roque Costa <devroque18@gmail.com>
2022-06-16 19:49:47 -0300

    Login update

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -12,7 +12,10 @@ export const Modal = () => {
             <div className="modal-container-flex">
                 <div className="modal-container">
                     <h1>Bem vindo ao iLunch</h1>
-                    <p>Aqui você pode matar sua fome de maneira fácil, rápido e barata!</p>
+                    <p>
+                        Aqui você pode matar sua fome de maneira fácil, rápido e barata!
+                        Faça seu cadastro e comece a aproveitar!    
+                    </p>
                     <button onClick={() => showModal(false)}>Vamos lá!</button>
                 </div>
             </div>
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import { Header } from '../../components/header/header';
+import { Link } from 'react-router-dom';
+import './index.css';
 
 export const Login = () => {
+    // const verifyInput = (type, event) => {
+    //     let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    //     console.log(emailRegex.test(event.target.value));
+    // }
+
     return (
-        <>
-            <Header logged={false} />
-            <div className="login-contaner">
-                <h2>Login</h2>
-                <div className="login-container-inputs">
-                    <input type="text" />
-                    <input type="password" />
-                    <span className="errors"></span>
+        <div className="login-container">
+            <div className="login">
+                <div className="login-header">
+                    <h1>Login</h1>
+                    <button><Link to="/">Inicio</Link></button>
+                </div>
+                <div className="login-inputs">
+                    <input 
+                        type="text" 
+                        placeholder="Email ou número de telefone" 
+                    />
+                    <input 
+                        type="password" 
+                        placeholder="Senha" 
+                    />
                     <button>Entrar</button>
+                    <button>Cadastrar</button>
+                    <Link to="/forget-login">Esqueci minha senha</Link>
                 </div>
             </div>
-        </>
+        </div>
     );
 }

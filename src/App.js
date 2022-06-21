000bb99d7ba056b0ff0d771662169b3153d8c695
Roque Costa <devroque18@gmail.com>
2022-06-21 10:03:03 -0300

    Auth routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
 import { Homepage } from './pages/homepage/home.js';
 import { RegisterPage } from './pages/register/index.js';
 import { StoreProfile } from './pages/store/index.js';
@@ -8,12 +9,16 @@ import { Login } from './pages/login/index.js';
 const App = () => {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Homepage />}></Route>
-        <Route path="/register" element={<RegisterPage />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/store-profile" element={<StoreProfile />}></Route>
-      </Routes>
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route exact path="/" element={<Homepage />}></Route>
+            <Route path="/register" element={<RegisterPage />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/store-profile" element={<StoreProfile />}></Route>
+          </Routes>
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
